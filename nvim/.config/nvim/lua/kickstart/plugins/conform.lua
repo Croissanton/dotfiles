908b36d214fa3037c7bcd3774e815cd50372bdd6
crisanton <crisruizmartin1@gmail.com>
2025-02-03 23:25:39 +0100

    couldnt get ruff to work as lsp and formatter :(, only used for linting atm

--- nvim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -34,7 +34,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'ruff' },
+        python = { 'black' },
         javascript = { 'prettierd' },
         bash = { 'beautysh' },
 
